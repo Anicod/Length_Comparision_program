18c7b092183be0e775804be92ed933dd72a15cdb
Anicod <animeshraj735@gmail.com>
2022-05-26 11:55:34 +0530

    added: comparision of the two lines

--- src/lengthcomparision/inputCoordinats.java
+++ src/lengthcomparision/inputCoordinats.java
@@ -3,9 +3,9 @@ package lengthcomparision;
 class inputCoordinats {
     int x1, y1, x2, y2;
 
-    void lineLength() {
-        double lineDistance;
+    Double lineLength() {
+        Double lineDistance;
         lineDistance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println(lineDistance);
+        return lineDistance;
     }
 }
